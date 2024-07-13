29d00d11ef12639a53a8b1c71fbb3659b1abb9cc
SUddinShakib <shakib.shamsuddin@gmail.com>
2024-07-13 19:48:52 +0600

    All Doctors and All Labs

---
+++ OnlineDiagnosticSystem/Controllers/PatientAppointController.cs
@@ -0,0 +1,35 @@
+﻿using DatabaseLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineDiagnosticSystem.Controllers
+{
+    public class PatientAppointController : Controller
+    {
+        private OnlineDiagnosticLabSystemDbEntities db = new OnlineDiagnosticLabSystemDbEntities();
+        public ActionResult GetAllDoctors()
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            var doclist = db.DoctorTables.Where(d => d.UserTable.isVerified == true);
+
+            return View(doclist);
+        }
+
+        public ActionResult GetAlllabs()
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            var lablist = db.LabTables.Where(d => d.UserTable.isVerified == true);
+
+            return View(lablist);
+        }
+    }
+}
