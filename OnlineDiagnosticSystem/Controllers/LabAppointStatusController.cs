5c9e5910ac2e18cda3951494fe6188c5ab180fad
SUddinShakib <shakib.shamsuddin@gmail.com>
2024-07-15 14:43:53 +0600

    Lab Appointment Status

---
+++ OnlineDiagnosticSystem/Controllers/LabAppointStatusController.cs
@@ -0,0 +1,70 @@
+﻿using DatabaseLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineDiagnosticSystem.Controllers
+{
+    public class LabAppointStatusController : Controller
+    {
+        private OnlineDiagnosticLabSystemDbEntities db = new OnlineDiagnosticLabSystemDbEntities();
+        // GET: DoctorAppointStatus
+        public ActionResult PendingAppoint()
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            var patient = (PatientTable)Session["Patient"];
+            var pendingappointment = db.LabAppointTables.Where(d => d.PatientID == patient.PatientID && d.IsComplete == false && d.IsFeeSubmit == false );
+            return View(pendingappointment);
+        }
+
+        public ActionResult CurrentAppoint()
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            var patient = (PatientTable)Session["Patient"];
+            var currentappointment = db.LabAppointTables.Where(d => d.PatientID == patient.PatientID && d.IsComplete == false && d.IsFeeSubmit == true);
+            return View(currentappointment);
+        }
+
+        public ActionResult CompleteAppoint()
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            var patient = (PatientTable)Session["Patient"];
+            var completeappointment = db.LabAppointTables.Where(d => d.PatientID == patient.PatientID && d.IsComplete == true && d.IsFeeSubmit == true);
+            return View(completeappointment);
+        }
+
+        public ActionResult CancelAppoint()
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            var patient = (PatientTable)Session["Patient"];
+            var cancelappointment = db.LabAppointTables.Where(d => d.PatientID == patient.PatientID &&( d.IsComplete == false || d.IsFeeSubmit == false)&& d.AppointDate < DateTime.Now); 
+            return View(cancelappointment);
+        }
+
+
+        public ActionResult AllAppoint()
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            var patient = (PatientTable)Session["Patient"];
+            var allappointment = db.LabAppointTables.Where(d => d.PatientID == patient.PatientID);
+            return View(allappointment);
+        }
+    }
+}
